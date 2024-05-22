1dc7390f371440820bca565e4bd0d3a28df8aaee
HC-Guo <32841216+HC-Guo@users.noreply.github.com>
2024-05-23 03:24:46 +0800

    Update html_simplifier.py

--- code/utils/html/html_simplifier.py
+++ code/utils/html/html_simplifier.py
@@ -35,26 +35,23 @@ def simplify(content="<html>...</html>"):
             if el is None or el == None:
                 continue
             souptmp = clean_soup
-            # 获取最上层节点的子节点
+
             children = el.findChildren()
-            # 从文档中删除最上层节点
+
             el.extract()
             combined_text = ''
-            # 遍历子节点并连接它们的文本内容
+
             for child in children:
                 combined_text += str(child)
-            # print('===删除后===')
-            # print(combined_text)
+       
             souptmp = souptmp.replace(str(el), combined_text)
-            # 对比临时HTML和原始HTML的渲染结果
-            # 获取当前时间并格式化为字符串
             timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")
-            # 创建一个临时的HTML文件用于测试
+
             tmp_html_file = f'{tmp_dir}tmphtml_{timestamp}.html'
-            # print(tmp_html_file)
+
             with open(tmp_html_file, 'w') as file:
                 file.write(str(souptmp))
-            # 创建tmp渲染图片，加上时间戳
+
             tmp_screenshot = f'{tmp_dir}tmp_image_{timestamp}.png'
 
             with sync_playwright() as playwright:
@@ -65,9 +62,8 @@ def simplify(content="<html>...</html>"):
                 page.close()
                 browser.close()
 
-            # 比较图片
             if filecmp.cmp(f'{tmp_dir}original.png', f'{tmp_screenshot}', shallow=False):
-                # 如果临时HTML的渲染结果与原始HTML相同，则soup = tmpsoup
+
                 clean_soup = souptmp
                 # print('-'*20, 'remove:', el.name)
                 removed_elements.append(el)
